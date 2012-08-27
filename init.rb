237574d96965aedee40373afca45ded77fcd267b
Alex Mekhovov <alex.mehovov@gmail.com>
2012-08-27 10:00:51 +0300

    first commit

---
+++ init.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+set :public_folder, File.dirname(__FILE__)
+set :haml, {:format => :html5, :attr_wrapper => '"'}
+
+get '/' do
+  File.read('index.html')
+end
+
+get '/qa' do
+  haml :qa, :layout => :qa_layout
+end
+
+get '/slide80' do
+  redirect '/qa'
+end
+
+get '/:slide' do
+  File.read('index.html')
+end
+
